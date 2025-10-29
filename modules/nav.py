89014bdc2c7118da07074c2b83e0ff26c2a3ef53
mohidqadeer123 <moqadeer@iu.edu>
2025-10-30 00:04:30 +0500

    Implement sidebar with image and links
    Add sidebar function to display image and links

--- modules/nav.py
+++ modules/nav.py
@@ -1,1 +1,25 @@
+from pathlib import Path
 
+def sidebar():
+    import streamlit as st
+
+    root = Path(__file__).resolve().parents[1]
+    img = root / "frontend" / "assets" / "marvelteam.jpg"
+
+
+    with st.sidebar:
+        if img.exists():
+            st.image(str(img), width="stretch")
+
+        st.sidebar.markdown("Music & Mental Health")
+        st.sidebar.caption("Group 4 - H501 Final Project")
+
+        st.sidebar.divider()
+
+        # Extras 
+        st.sidebar.markdown("Links")
+        st.page_link(
+            "https://www.kaggle.com/code/melissamonfared/mental-health-music-relationship-analysis-eda",
+            label = "Kaggle Notebook",
+            icon = "🔗",
+            )
